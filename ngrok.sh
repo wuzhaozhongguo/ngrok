784235a0d0370a134f6af2aa07d29653003075d1
wuzhaozhongguo <13991544720@139.com>
2019-08-26 10:12:25 +0800

    ig

--- ngrok.sh
+++ ngrok.sh
@@ -82,8 +82,8 @@ install_ngrok(){
 	uninstall_ngrok
 	_NGROK_BASE_PATH="/usr/local/ngrok/ngrok-server/"
 	cd /usr/local
-	if [ ! -f "${_NGROK_BASE_PATH}/ngrok.zip" ];then
-		cd ${_NGROK_BASE_PATH}
+	if [ ! -f "${_NGROK_BASE_PATH}/ngrok/ngrok.zip" ];then
+		cd ${_NGROK_BASE_PATH}ngrok
 		wget http://www.sunnyos.com/ngrok.zip
 	fi
 	unzip ngrok.zip
